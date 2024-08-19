5f44323e2017d9c4fd706905523f2816789b871d
Sovereign Shahid <sovereignshahid@gmail.com>
2024-08-19 14:52:34 -0600

    re-ennabled paging setup

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -26,9 +26,8 @@ void kernel_main(uint32_t magic, struct multiboot_info* bootInfo) {
 	printf("Welcome to beanOS!\n");
 	initIdt();
 
-	//uint32_t mod1 = *(uint32_t*)(bootInfo->mods_addr + 4);
-	//uint32_t physicalAllocStart = (mod1 + 0xFFF) & ~0xFFF;
+	uint32_t mod1 = *(uint32_t*)(bootInfo->mods_addr + 4);
+	uint32_t physicalAllocStart = (mod1 + 0xFFF) & ~0xFFF;
 
-	//initMemory(bootInfo->mem_upper * 1024, physicalAllocStart);
-
+	initMemory(bootInfo->mem_upper * 1024, physicalAllocStart);
 }
