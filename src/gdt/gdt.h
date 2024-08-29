0e4d356d9a96e14793f4970277f9f147f1c05842
Sovereign Shahid <sovereignshahid@gmail.com>
2024-08-28 20:42:11 -0600

    initialized gdt (without system segment)

---
+++ src/gdt/gdt.h
@@ -0,0 +1,71 @@
+#ifndef GDT_H
+#define GDT_H
+#include <stdint.h>
+
+struct gdt_entry_struct {
+	uint16_t limit;
+	uint16_t base_low;
+	uint8_t base_middle;
+	uint8_t access;
+	uint8_t flags;
+	uint8_t base_high;
+} __attribute__((packed));
+
+struct gdt_entry_bits {
+	unsigned int limit_low              : 16;
+	unsigned int base_low               : 24;
+	unsigned int accessed               :  1;
+	unsigned int read_write             :  1; // readable for code, writable for data
+	unsigned int conforming_expand_down :  1; // conforming for code, expand down for data
+	unsigned int code                   :  1; // 1 for code, 0 for data
+	unsigned int code_data_segment      :  1; // should be 1 for everything but TSS and LDT
+	unsigned int DPL                    :  2; // privilege level
+	unsigned int present                :  1;
+	unsigned int limit_high             :  4;
+	unsigned int available              :  1; // only used in software; has no effect on hardware
+	unsigned int long_mode              :  1;
+	unsigned int big                    :  1; // 32-bit opcodes for code, uint32_t stack for data
+	unsigned int gran                   :  1; // 1 to use 4k page addressing, 0 for byte addressing
+	unsigned int base_high              :  8;
+} __attribute__((packed));
+
+struct gdt_ptr_struct {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed));
+
+struct tss_entry_struct {
+	uint32_t prev_tss;
+	uint32_t esp0;
+	uint32_t ss0;
+	uint32_t esp1;
+	uint32_t ss1;
+	uint32_t esp2;
+	uint32_t ss2;
+	uint32_t cr3;
+	uint32_t eip;
+	uint32_t eflags;
+	uint32_t eax;
+	uint32_t ecx;
+	uint32_t edx;
+	uint32_t ebx;
+	uint32_t esp;
+	uint32_t ebp;
+	uint32_t esi;
+	uint32_t edi;
+	uint32_t es;
+	uint32_t cs;
+	uint32_t ss;
+	uint32_t ds;
+	uint32_t fs;
+	uint32_t gs;
+	uint32_t ldt;
+	uint32_t trap;
+	uint32_t iomap_base;
+
+} __attribute__((packed));
+
+void initGdt();
+void setGdtGate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran);
+void writeTSS(uint32_t num, uint16_t ss0, uint32_t esp0);
+#endif
